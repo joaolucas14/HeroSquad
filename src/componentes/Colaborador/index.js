c15916594c56c5c83048851752771e6c28b52758
joaolucas14 <jlparceiro10@gmail.com>
2024-10-09 18:04:05 -0300

    criando os colaboradores e adicionando as equipes corretas

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,15 +1,15 @@
 import "./Colaborador.css";
 
-const Colaborador = () => {
+const Colaborador = ({ nome, imagem, cargo }) => {
   return (
     <div className="colaborador">
       <div className="cabecalho">
-        <img src="https://github.com/joaolucas14.png" alt="João Lucas" />
+        <img src={imagem} alt={nome} />
       </div>
 
       <div className="rodape">
-        <h4>João Lucas</h4>
-        <h5>Estagiário</h5>
+        <h4>{nome}</h4>
+        <h5>{cargo}</h5>
       </div>
     </div>
   );
